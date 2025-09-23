e5db811509b59f910620e3e849e6343c95bd6a44
jianhe <jianhee@qq.com>
2025-09-23 11:48:59 +0800

    update

--- stylelint/modules/common.js
+++ stylelint/modules/common.js
@@ -17,6 +17,9 @@ module.exports = {
     // 字体列表中是否缺少通用系列：不校验
     'font-family-no-missing-generic-family-keyword': null,
     // 是否允许特异性较低的选择器在特异性较高的选择器之后出现：不校验
-    'no-descending-specificity': null
+    'no-descending-specificity': null,
+    // 类名校验规则：必须以小写字母开头，后跟字母、数字、短横线、短横线
+    // 例如：.block, .block123, .block-modifier, .block--modifier, .block__element, .block__element--modifier
+    'selector-class-pattern': ['^[a-z][a-z0-9-_]+$']
   }
 };
